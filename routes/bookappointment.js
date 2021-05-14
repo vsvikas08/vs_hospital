2e37fa498d63cef868fc73da1c1efb87ea1683ce
Your Name <vikas3517@gmail.com>
2021-05-14 20:51:38 +0530

    consult online and lab test form created

--- routes/bookappointment.js
+++ routes/bookappointment.js
@@ -1,8 +1,15 @@
 const route = require('express').Router()
 
 route.get('/',(req,res)=>{
-	res.render('bookappointment.ejs')
-	
+	res.render('bookappointment.ejs')	
+})
+
+route.get('/online',(req,res)=>{
+	res.render('consultonline.ejs')
+})
+
+route.get('/lab',(req,res)=>{
+	res.render('booklab.ejs')
 })
 
 
